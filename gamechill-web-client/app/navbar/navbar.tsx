ab4931fa62e761338f389b30dbe3e92493b8c932
ahmadbasyouni10 <ahmadnayef2004@gmail.com>
2024-02-14 12:58:57 -0500

    Implemented user authentication using firebase auth, by using react hooks to observe state changing. Sign In and Sign Out button rendering (Client Side). Used google Firebase SDK and Google Provider.

--- gamechill-web-client/app/navbar/navbar.tsx
+++ gamechill-web-client/app/navbar/navbar.tsx
@@ -1,8 +1,34 @@
+'use client';
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./navbar.module.css";
+import SignIn from "./signin";
+import { onAuthStateChangedHelper } from "../utilities/firebase";
+import { useEffect, useState } from 'react';
+import { unsubscribe } from "diagnostics_channel";
+import { User } from "firebase/auth";
 
+/*State determined within parent Navbar, so can be used for
+sign button and Upload Button. 2 hooks used function hook
+useState null init. so give type explicitly
+*/
 export default function Navbar() {
+    //Defines state var. which will have current user using hook
+    const [user, setUser] = useState< User | null >(null);
+    
+    //Another hook subscribed to onAuthStateChangedHelper
+    //When authenticationState changes, updates state var with user
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user)=>{ 
+            setUser(user);
+        });
+
+        //Avoid memory leak
+        return () => unsubscribe();
+
+
+    });
+
     return(
         <nav className={styles.nav}>
             <Link style={{textDecoration: 'none'}}href="/">
@@ -17,6 +43,7 @@ export default function Navbar() {
                     <p className={styles.logoText}>GameChill</p>
                 </div>
             </Link>
+            <SignIn user = {user} />
         </nav>
     )
 }
